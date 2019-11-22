62865fcf65ed10cf5a508e348f3ba9bbd8736714
Даниил Жданкин <dszhdankin@edu.hse.ru>
2019-11-22 19:59:35 +0300

    Tests and workflow

--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ from http.server import HTTPServer
 from io import BytesIO
 from threading import Thread
 from typing import Optional
+from multiprocessing import Process
 from zipfile import ZipFile
+from flask_server import app
 
 from compressors import ZIP_COMPRESSOR_TYPES, ZipCompressorFactory
 from server import CompressionHTTPRequestHandler
@@ -73,26 +75,36 @@ def append_test_file_path(file_name: str) -> str:
     return "test-files/%s" % file_name
 
 
-# noinspection DuplicatedCode
-class TestServer(unittest.TestCase):
-    PORT = 8887
-    httpd: Optional[HTTPServer] = None
-    thread: Optional[Thread] = None
+class TestFlaskServer(unittest.TestCase):
+    server: Optional[Process] = None
+    PORT = 8080
 
     def setUp(self):
-        def create_server():
-            with HTTPServer(("", self.PORT), CompressionHTTPRequestHandler) as httpd:
-                self.httpd = httpd
-                httpd.serve_forever()
-
-        self.thread = Thread(target=create_server)
-        self.thread.start()
-
-        time.sleep(2)  # Give the server some time to start
+        def run_server():
+            app.run(host='127.0.0.1', port=self.PORT)
+        self.server = Process(target=run_server)
+        self.server.start()
 
     def tearDown(self):
-        self.httpd.shutdown()
-        self.thread.join()
+        self.server.terminate()
+        self.server.join()
+
+    # def testResponnce(self):
+    #     subprocess.run(
+    #         args='curl -F "file=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {type}'.format(
+    #             dir=os.getcwd(),
+    #             file=append_test_file_path('text.txt'),
+    #             port=self.PORT,
+    #             type='zipStored',
+    #         ),
+    #         shell=True,
+    #     )
+    #     archive_file = ZipFile(compressor_name, mode="r", compression=compression_type)
+    #     archived_file_content = archive_file.read(file_name)
+    #     self.assertEqual(archived_file_content, file_content,
+    #                      msg="File %s, compressor %s" % (file_name, compressor_name))
+    #     os.remove(compressor_name)
+
 
     def test_file_savings(self):
         for file_name in ("text.txt", "text-empty.txt", "large-file.bin"):
@@ -101,9 +113,10 @@ class TestServer(unittest.TestCase):
                 for compressor_name, compression_type in ZIP_COMPRESSOR_TYPES.items():
                     with open(os.devnull, "w") as null:
                         subprocess.run(
-                            args='curl -F "file=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {type}'.format(
+                            args='curl -F "{file_name}=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {type}'.format(
                                 dir=os.getcwd(),
                                 file=append_test_file_path(file_name),
+                                file_name = file_name,
                                 port=self.PORT,
                                 type=compressor_name,
                             ),
@@ -126,10 +139,11 @@ class TestServer(unittest.TestCase):
                 out_file = "%s-%s" % (compressor_name, thread_id)
                 with open(os.devnull, "w") as null:
                     subprocess.run(
-                        args='curl -F "file=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {out}'.format(
+                        args='curl -F "{file_name}=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {out}'.format(
                             dir=os.getcwd(),
                             file=append_test_file_path(file_name),
                             port=self.PORT,
+                            file_name = file_name,
                             type=compressor_name,
                             out=out_file,
                         ),
@@ -168,6 +182,103 @@ class TestServer(unittest.TestCase):
         for request in requests:
             request[0].join()
             self.assertTrue(request[1])
+
+
+# noinspection DuplicatedCode
+# class TestServer(unittest.TestCase):
+#     PORT = 8080
+#     httpd: Optional[HTTPServer] = None
+#     thread: Optional[Thread] = None
+#
+#     def setUp(self):
+#         def create_server():
+#             with HTTPServer(("", self.PORT), CompressionHTTPRequestHandler) as httpd:
+#                 self.httpd = httpd
+#                 httpd.serve_forever()
+#
+#         self.thread = Thread(target=create_server)
+#         self.thread.start()
+#
+#         time.sleep(2)  # Give the server some time to start
+#
+#     def tearDown(self):
+#         self.httpd.shutdown()
+#         self.thread.join()
+#
+#     def test_file_savings(self):
+#         for file_name in ("text.txt", "text-empty.txt", "large-file.bin"):
+#             with open(append_test_file_path(file_name), "rb") as opened_file:
+#                 file_content = opened_file.read()
+#                 for compressor_name, compression_type in ZIP_COMPRESSOR_TYPES.items():
+#                     with open(os.devnull, "w") as null:
+#                         subprocess.run(
+#                             args='curl -F "file=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {type}'.format(
+#                                 dir=os.getcwd(),
+#                                 file=append_test_file_path(file_name),
+#                                 port=self.PORT,
+#                                 type=compressor_name,
+#                             ),
+#                             stdout=null,
+#                             stderr=null,
+#                             shell=True,
+#                         )
+#
+#                     archive_file = ZipFile(compressor_name, mode="r", compression=compression_type)
+#                     archived_file_content = archive_file.read(file_name)
+#                     self.assertEqual(archived_file_content, file_content,
+#                                      msg="File %s, compressor %s" % (file_name, compressor_name))
+#                     os.remove(compressor_name)
+#
+#     def test_simultaneous_requests(self):
+#         def request_archive(thread_return: list, file_name: str, compressor_name: str, compression_type: str,
+#                             thread_id: int):
+#             with open(append_test_file_path(file_name), "rb") as opened_file:
+#                 file_content = opened_file.read()
+#                 out_file = "%s-%s" % (compressor_name, thread_id)
+#                 with open(os.devnull, "w") as null:
+#                     subprocess.run(
+#                         args='curl -F "file=@{dir}/{file}" http://localhost:{port}/convert/{type} -o {out}'.format(
+#                             dir=os.getcwd(),
+#                             file=append_test_file_path(file_name),
+#                             port=self.PORT,
+#                             type=compressor_name,
+#                             out=out_file,
+#                         ),
+#                         stdout=null,
+#                         stderr=null,
+#                         shell=True,
+#                     )
+#
+#                 archive_file = ZipFile(out_file, mode="r", compression=compression_type)
+#                 archived_file_content = archive_file.read(file_name)
+#                 thread_return.append(archived_file_content == file_content)
+#                 os.remove(out_file)
+#
+#         requests_count = 2
+#         requests = list()
+#
+#         for compressor_name, compression_type in ZIP_COMPRESSOR_TYPES.items():
+#             for thread_id in range(requests_count):
+#                 for file_name in ("text.txt", "text-empty.txt", "large-file.bin"):
+#                     thread_return = list()
+#                     thread = Thread(target=request_archive,
+#                                     args=(
+#                                         thread_return,
+#                                         file_name,
+#                                         compressor_name,
+#                                         compression_type,
+#                                         len(requests),
+#                                     ))
+#                     thread_return.append(thread)
+#
+#                     requests.append(thread_return)
+#
+#         for request in requests:
+#             request[0].start()
+#
+#         for request in requests:
+#             request[0].join()
+#             self.assertTrue(request[1])
 
 
 if __name__ == '__main__':
